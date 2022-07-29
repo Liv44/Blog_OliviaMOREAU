f51903f1991fe35805e2fe8723764f3aab8d03be
Olivia MOREAU <73989303+Liv44@users.noreply.github.com>
2022-07-29 18:26:37 +0200

    Revert "Feature/liv 20 tabs"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,8 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import "@fontsource/fraunces";
-import "@fontsource/open-sans";
-import { theme } from "../theme/index";
-import Layout from "../components/general/layout/layout";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
