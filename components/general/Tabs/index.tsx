8db0c1145b442c851ac5f319a253fbbb5b784e9c
Liv44 <moreauolivia49@gmail.com>
2022-07-29 18:53:17 +0200

    FIX(errors)

---
+++ components/general/Tabs/index.tsx
@@ -0,0 +1,51 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { FC } from "react";
+
+export interface TabsProps {
+  tabs: TabProps[];
+}
+export interface TabProps {
+  headerTitle: string;
+  content: React.ReactNode;
+}
+
+const TabsComponent: FC<TabsProps> = ({ tabs }) => {
+  return (
+    <Tabs
+      m={12}
+      colorScheme={"blue.500"}
+      variant="enclosed"
+      isFitted
+      border={"1px"}
+      borderColor="blue.500"
+      borderRadius={20}
+      bg={"white"}
+      boxShadow="lg"
+    >
+      <TabList>
+        {tabs.map((tab) => (
+          <Tab
+            _selected={{
+              color: "white",
+              bg: "blue.500",
+            }}
+            borderTopLeftRadius={18}
+            borderTopRightRadius={18}
+            key={tab.headerTitle}
+            fontSize={"xs"}
+          >
+            {tab.headerTitle}
+          </Tab>
+        ))}
+      </TabList>
+
+      <TabPanels>
+        {tabs.map((tab) => (
+          <TabPanel key={tab.headerTitle}>{tab.content}</TabPanel>
+        ))}
+      </TabPanels>
+    </Tabs>
+  );
+};
+
+export default TabsComponent;
