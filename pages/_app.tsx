5ffe0e5623d943b2de8e53ad01c9ac177f23f140
Liv44 <moreauolivia49@gmail.com>
2022-08-05 22:22:03 +0200

    feat(metadonnees): Balise Head added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,19 @@ import "@fontsource/fraunces";
 import "@fontsource/open-sans";
 import { theme } from "../theme/index";
 import Layout from "../components/general/layout/layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Olivia MOREAU - Développeuse Web</title>
+        <meta
+          name="description"
+          content="Blog/portfolio d'une développeuse web, créé avec Next.js et Chakra-ui"
+        />
+        <link rel="icon" href="/profile_picture.png" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
