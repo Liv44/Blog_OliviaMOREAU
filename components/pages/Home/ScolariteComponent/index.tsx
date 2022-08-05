930806352210c5ca291d42ce0de40cb977033ae4
Liv44 <moreauolivia49@gmail.com>
2022-08-05 20:30:26 +0200

    feat(title scolarite): add a title to the component scolarite

--- components/pages/Home/ScolariteComponent/index.tsx
+++ components/pages/Home/ScolariteComponent/index.tsx
@@ -1,26 +1,27 @@
 import { Box, Flex, HStack, Image, Stack } from "@chakra-ui/react";
 import { FC } from "react";
 import TabsComponent, { TabsProps } from "../../../general/Tabs";
+import Title from "../../../general/Title";
 
 const ScolariteComponent: FC<TabsProps> = ({ tabs }) => {
   return (
-    <Flex
-      direction={{ base: "column", md: "column", lg: "row" }}
-      justifyContent="space-around"
-      p={{ base: "8", md: "5", lg: "5" }}
-      alignItems="center"
-      bg={"blue.100"}
-      gap={4}
-      w="100%"
-    >
-      <Image
-        width={{ base: "300px", md: "400px", lg: "450px" }}
-        src="/cuate.png"
-        alt="education—Image"
-      ></Image>
+    <Stack bg={"blue.100"} w="100%" p={{ base: "8", md: "5", lg: "5" }}>
+      <Title title="Ma scolarité"></Title>
+      <Flex
+        direction={{ base: "column", md: "column", lg: "row" }}
+        justifyContent="space-around"
+        alignItems="center"
+        gap={4}
+      >
+        <Image
+          width={{ base: "300px", md: "400px", lg: "450px" }}
+          src="/cuate.png"
+          alt="education—Image"
+        ></Image>
 
-      <TabsComponent tabs={tabs}></TabsComponent>
-    </Flex>
+        <TabsComponent tabs={tabs}></TabsComponent>
+      </Flex>
+    </Stack>
   );
 };
 
