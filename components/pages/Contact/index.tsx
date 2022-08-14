f54a4c3e7204e283626d761d42c6c0a77f4c1594
Liv44 <moreauolivia49@gmail.com>
2022-08-14 03:57:56 +0200

    feat(changes on contact): delete form and add img

--- components/pages/Contact/index.tsx
+++ components/pages/Contact/index.tsx
@@ -2,210 +2,84 @@ import {
   GithubFilled,
   LinkedinFilled,
   MailOutlined,
-  PropertySafetyOutlined,
-  TwitterOutlined,
-  UserOutlined,
+  TwitterCircleFilled,
 } from "@ant-design/icons";
-import { EmailIcon } from "@chakra-ui/icons";
-import {
-  Container,
-  Flex,
-  Box,
-  Heading,
-  Text,
-  Button,
-  VStack,
-  Wrap,
-  WrapItem,
-  FormControl,
-  FormLabel,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  Textarea,
-} from "@chakra-ui/react";
-import { FC, useState } from "react";
+import { Link, VStack, Text, Icon, Flex, Image, Stack } from "@chakra-ui/react";
+import { FC } from "react";
+import Title from "../../general/Title";
 
 const ContactCard: FC = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = async (e: any) => {
-    const formData = { name, email, message };
-    console.log(name, email, message);
-    await fetch("/api/mail", {
-      method: "POST",
-      body: JSON.stringify(formData),
-    });
-  };
-
   return (
-    <Container maxW="full" mt={0} centerContent overflow="hidden">
-      <Flex>
-        <Box
-          bg={"orange.600"}
-          color="white"
-          borderRadius="lg"
-          m={{ sm: 4, md: 16, lg: 10 }}
-          p={{ sm: 5, md: 5, lg: 16 }}
+    <VStack>
+      <Title
+        title="Me contacter"
+        subTitle="Vous pouvez me contacter sur mes réseaux sociaux ou par mail."
+      ></Title>
+      <Stack direction={{ base: "column", md: "row" }} alignItems={"center"}>
+        <Stack
+          alignItems={"flex-start"}
+          gap={3}
+          justifyContent={{ base: "center", md: "flex-start" }}
+          direction={{ base: "row", md: "column" }}
+          flexWrap={"wrap"}
         >
-          <Box p={4}>
-            <Wrap spacing={{ base: 10, sm: 3, md: 5, lg: 10 }}>
-              <WrapItem>
-                <Box>
-                  <Heading>Contact</Heading>
-                  <Box py={{ base: 5, sm: 5, md: 8, lg: 5 }}>
-                    <VStack pl={0} spacing={3} alignItems="flex-start">
-                      <Button
-                        size="md"
-                        variant={"outline"}
-                        height="48px"
-                        width="250px"
-                        colorScheme="white"
-                        as="a"
-                        href="mailto:moreauolivia49@gmail.com"
-                        target="_blank"
-                        _hover={{
-                          bg: "white",
-                          borderColor: "blue",
-                          color: "blue",
-                        }}
-                        leftIcon={<MailOutlined color="#1970F1" />}
-                      >
-                        moreauolivia49@gmail.com
-                      </Button>
-                      <Button
-                        size="md"
-                        variant={"outline"}
-                        height="48px"
-                        width="150px"
-                        colorScheme="white"
-                        as="a"
-                        href="https://linkedin.com/in/oliviamoreau"
-                        target="_blank"
-                        _hover={{
-                          bg: "white",
-                          borderColor: "blue",
-                          color: "blue",
-                        }}
-                        leftIcon={<LinkedinFilled color="#1970F1" />}
-                      >
-                        Olivia Moreau
-                      </Button>
-                      <Button
-                        size="md"
-                        variant={"outline"}
-                        height="48px"
-                        width="100px"
-                        colorScheme="white"
-                        as="a"
-                        href="https://github.com/liv44"
-                        target="_blank"
-                        _hover={{
-                          bg: "white",
-                          borderColor: "blue",
-                          color: "blue",
-                        }}
-                        leftIcon={<GithubFilled color="#1970F1" />}
-                      >
-                        Liv44
-                      </Button>
-                      <Button
-                        size="md"
-                        variant={"outline"}
-                        height="48px"
-                        width="100px"
-                        colorScheme="white"
-                        as="a"
-                        href="https://twitter.com/liivDev"
-                        target="_blank"
-                        _hover={{
-                          bg: "white",
-                          borderColor: "blue",
-                          color: "blue",
-                        }}
-                        leftIcon={<TwitterOutlined />}
-                      >
-                        @liivDev
-                      </Button>
-                    </VStack>
-                  </Box>
-                </Box>
-              </WrapItem>
-              <WrapItem>
-                <Box bg="white" borderRadius="lg">
-                  <Box m={8} color="grey.700">
-                    <VStack spacing={5}>
-                      <FormControl id="name">
-                        <FormLabel>Votre nom</FormLabel>
-                        <InputGroup borderColor="#E0E1E7">
-                          <InputLeftElement pointerEvents="none">
-                            <UserOutlined></UserOutlined>
-                          </InputLeftElement>
-                          <Input
-                            type="text"
-                            size="md"
-                            fontSize="sm"
-                            onChange={(e) => {
-                              setName(e.target.value);
-                            }}
-                            name="name"
-                          />
-                        </InputGroup>
-                      </FormControl>
-                      <FormControl id="name">
-                        <FormLabel>Mail</FormLabel>
-                        <InputGroup borderColor="#E0E1E7" size="lg">
-                          <InputLeftElement pointerEvents="none">
-                            <EmailIcon></EmailIcon>
-                          </InputLeftElement>
-                          <Input
-                            fontSize="sm"
-                            type="text"
-                            size="md"
-                            onChange={(e) => {
-                              setEmail(e.target.value);
-                            }}
-                            name="email"
-                          />
-                        </InputGroup>
-                      </FormControl>
-                      <FormControl id="name">
-                        <FormLabel>Message</FormLabel>
-                        <Textarea
-                          fontSize="sm"
-                          placeholder="Votre message"
-                          borderColor="gray.300"
-                          p={2}
-                          _hover={{
-                            borderRadius: "gray.300",
-                          }}
-                          onChange={(e) => {
-                            setMessage(e.target.value);
-                          }}
-                          name="message"
-                        />
-                      </FormControl>
-                      <FormControl id="name" float="right">
-                        <Button
-                          variant="solid"
-                          colorScheme="blue"
-                          _hover={{}}
-                          onClick={(e) => handleSubmit(e.target)}
-                        >
-                          Send Message
-                        </Button>
-                      </FormControl>
-                    </VStack>
-                  </Box>
-                </Box>
-              </WrapItem>
-            </Wrap>
-          </Box>
-        </Box>
-      </Flex>
-    </Container>
+          <ItemContact
+            link="https://github.com/liv44"
+            text="Liv44"
+            icon={GithubFilled}
+          ></ItemContact>
+          <ItemContact
+            link="https://www.linkedin.com/in/oliviamoreau/"
+            text="Olivia MOREAU"
+            icon={LinkedinFilled}
+          ></ItemContact>
+          <ItemContact
+            link="https://twitter.com/liivDev"
+            text="@liivDev"
+            icon={TwitterCircleFilled}
+          ></ItemContact>
+          <ItemContact
+            link="mailto:moreauolivia49@gmail.com"
+            text="moreauolivia49@gmail.com"
+            icon={MailOutlined}
+          ></ItemContact>
+        </Stack>
+        <Image
+          height={{ base: "36vh", md: "61vh" }}
+          alt={"Contact Image"}
+          objectFit={"cover"}
+          src={"./contact.png"}
+        />
+      </Stack>
+    </VStack>
   );
 };
 export default ContactCard;
+
+interface ItemContactProps {
+  icon: any;
+  text: string;
+  link: string;
+}
+
+const ItemContact: FC<ItemContactProps> = ({ icon, text, link }) => {
+  return (
+    <Link
+      href={link}
+      isExternal
+      display="flex"
+      alignItems={"center"}
+      gap={3}
+      padding={3}
+      borderRadius={20}
+      bg="white"
+      color="blue"
+      size={"lg"}
+      fontSize={"xl"}
+      _hover={{ textDecoration: "none", color: "orange.500" }}
+    >
+      <Icon as={icon} color={"blue"}></Icon>
+      <Text>{text}</Text>
+    </Link>
+  );
+};
