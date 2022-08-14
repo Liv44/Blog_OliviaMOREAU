171d432d6cf861a119142c7f46ca61248d0654ae
Liv44 <moreauolivia49@gmail.com>
2022-08-14 04:20:46 +0200

    feat(components blog): creation components

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,11 +1,17 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import Title from "../components/general/Title";
+import ArticleList from "../components/pages/Blog/ArticleList";
 
 const Blog: NextPage = () => {
   return (
-    <Box h="75vh">
-      <Heading>Blog en cours de réalisation.</Heading>
-    </Box>
+    <Stack>
+      <Title
+        title="Mon Blog"
+        subTitle="Vous retrouverez ici mes articles réalisés dans le cadre de mon rendu de 2ème année de Bachelor Informatique."
+      ></Title>
+      <ArticleList></ArticleList>
+    </Stack>
   );
 };
 
